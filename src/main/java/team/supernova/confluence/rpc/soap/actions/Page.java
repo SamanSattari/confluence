eb97f26a169121668b67d234de7ddaac5aaec142
Christopher Reedijk <Christopher.Reedijk@ing.nl>
2015-08-04 21:28:52 +0200

    Initial commit

---
+++ src/main/java/team/supernova/confluence/rpc/soap/actions/Page.java
@@ -0,0 +1,49 @@
+package team.supernova.confluence.rpc.soap.actions;
+
+import javax.xml.rpc.ServiceException;
+
+import lombok.Getter;
+import lombok.Setter;
+import team.supernova.confluence.rpc.soap.beans.RemotePageUpdateOptions;
+import team.supernova.confluence.rpc.soap.beans.RemotePage;
+
+import java.rmi.RemoteException;
+
+@Getter
+@Setter
+public class Page {
+    private static Token token;
+
+    public Page() throws ServiceException, RemoteException {
+        token = Token.getInstance();
+    }
+
+    public RemotePage read(String spaceName, String pageName) throws RemoteException {
+        RemotePage page = token.getService().getPage(token.getToken(), spaceName, pageName);
+        return page;
+    }
+
+    public RemotePage read(Long id) throws RemoteException {
+        RemotePage page = token.getService().getPage(token.getToken(), id);
+        return page;
+    }
+
+    public RemotePage update(RemotePage page) throws java.rmi.RemoteException, ServiceException {
+        RemotePageUpdateOptions rpuo = new RemotePageUpdateOptions();
+        rpuo.isMinorEdit();
+
+        return token.getService().updatePage(token.getToken(), page, rpuo);
+    }
+
+    public RemotePage store(RemotePage page) throws java.rmi.RemoteException, ServiceException {
+        return token.getService().storePage(token.getToken(), page);
+    }
+
+    public Boolean move(RemotePage page, String position) throws RemoteException {
+        return token.getService().movePage(token.getToken(), page.getId(), page.getId(), position);
+    }
+
+    public Boolean remove(Long id) throws RemoteException {
+        return token.getService().removePage(token.getToken(), id);
+    }
+}
---
+++ src/test/java/team/supernova/confluence/ConfluenceTest.java
@@ -0,0 +1,65 @@
+package team.supernova.confluence;
+
+import lombok.Getter;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import team.supernova.confluence.plugins.servlet.soap_axis.confluenceservice.ConfluenceSoapServiceServiceLocator;
+import team.supernova.confluence.rpc.soap.actions.Page;
+import team.supernova.confluence.rpc.soap.actions.Token;
+import team.supernova.confluence.rpc.soap.beans.RemotePage;
+
+import javax.xml.rpc.ServiceException;
+import java.rmi.RemoteException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+/**
+ * Created by m05f757 on 4-8-2015.
+ */
+public class ConfluenceTest {
+    private static final Logger LOG = Logger.getLogger(ConfluenceTest.class.toString());
+    private static final String TEST_PROPS_FILE = "test.properties";
+    private final static String PROJECT = "kaas";
+    private final static String TARGET_PAGE = "LLDS_1";
+    @Getter
+    private Properties testProperties;
+
+    @BeforeSuite
+    public void initializeConfluence() throws ServiceException, RemoteException {
+        try {
+            testProperties = new Properties();
+            testProperties.load(this.getClass().getClassLoader().getResourceAsStream(TEST_PROPS_FILE));
+        }
+        catch (Exception e) {
+            LOG.severe(String.format(
+                    "fatal error retrieving test properties [%s]: %s",
+                    TEST_PROPS_FILE,
+                    e.getMessage()));
+
+            throw new RuntimeException(e);
+        }
+
+        String confluenceUser = getTestProperties().getProperty("confluence.user");
+        String confluencePassword = getTestProperties().getProperty("confluence.password");
+        String endpointAddress = getTestProperties().getProperty("confluence.endpointaddress");
+
+        Token token = Token.getInstance();
+        token.initialise(confluenceUser, confluencePassword, endpointAddress);
+    }
+
+    @Test
+    public void testReadPage(){
+        try {
+            Page page = new Page();
+
+            RemotePage utPage = page.read(PROJECT, TARGET_PAGE);
+            LOG.info(utPage.getContent().toString());
+        } catch (RemoteException e) {
+            LOG.severe(e.getMessage());
+        } catch (ServiceException e) {
+            LOG.severe(e.getMessage());
+        }
+    }
+
+}
